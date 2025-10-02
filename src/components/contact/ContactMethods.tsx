21163ef598792ea24282fc9fd3e24b3f7e8018d8
P1rey <renshao@iu.edu>
2025-10-02 00:23:40 -0400

    feat: update RedNote QR code to latest version
    - Replace RedNote QR code with new image from desktop
    - Add rednote-qr-latest.png (31KB, higher resolution)
    - Update ContactMethods to use latest RedNote QR code
    - Provides users with the most current RedNote (å°çº¢ä¹¦) contact information

--- src/components/contact/ContactMethods.tsx
+++ src/components/contact/ContactMethods.tsx
@@ -10,7 +10,7 @@ function getPublicAsset(path: string): string {
 // QR code URLs
 const QR_CODES = {
   wechat: getPublicAsset('/qr-codes/wechat-qr-latest.png'),
-  rednote: getPublicAsset('/qr-codes/rednote-qr-cropped.png'),
+  rednote: getPublicAsset('/qr-codes/rednote-qr-latest.png'),
   bilibili: getPublicAsset('/qr-codes/bilibili-qr.svg')
 };
 
