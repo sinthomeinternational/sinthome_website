974ef36c9aa2e69b8c5975ceab0fe0ddf84e8a2e
P1rey <renshao@iu.edu>
2025-10-02 01:22:18 -0400

    feat: update Bilibili QR code to latest version
    - Replace Bilibili QR code with new image from desktop
    - Add bilibili-qr-latest.png (32KB, higher resolution)
    - Update ContactMethods to use latest Bilibili QR code
    - Now all three platforms (WeChat, RedNote, Bilibili) use latest PNG QR codes

--- src/components/contact/ContactMethods.tsx
+++ src/components/contact/ContactMethods.tsx
@@ -11,7 +11,7 @@ function getPublicAsset(path: string): string {
 const QR_CODES = {
   wechat: getPublicAsset('/qr-codes/wechat-qr-latest.png'),
   rednote: getPublicAsset('/qr-codes/rednote-qr-latest.png'),
-  bilibili: getPublicAsset('/qr-codes/bilibili-qr.svg')
+  bilibili: getPublicAsset('/qr-codes/bilibili-qr-latest.png')
 };
 
 export default function ContactMethods() {
